a9bf22a6a30ac10e01b281d6d084e38e3b445a14
Jaled Breyaui <jaledbreyaui41@gmail.com>
2022-10-06 15:50:18 -0300

    login

---
+++ logIn/server.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const session = require('express-session')
+const MongoStore = require('connect-mongo')
+const advancedOptions = { useNewUrlParser: true, useUnifiedTopology: true }
+const routerSession = express.Router()
+const app = express()
+require('dotenv').config()
+const authMiddleware = require('./middleware/auth')
+
+
+app.use(express.static(__dirname + '/public'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.set('view engine', 'ejs')
+app.set('views', './views')
+
+app.use(session({
+    secret: '1234',
+    saveUninitialized: true,
+    resave: true,
+    store: MongoStore.create({
+        mongoUrl: `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}${process.env.MONGO_HOST}/test`,
+        mongoOptions: advancedOptions
+    })
+}))
+
+
+
+routerSession.get('/', (req, res) => {
+    res.status(200).sendFile(__dirname + '/public/index.html')
+})
+
+routerSession.post('/login', async (req, res) => {
+    try {
+        const { usuario } = req.body
+        req.session.usuario = usuario
+        if (usuario == 'Jaled') {
+            res.redirect('/session/logueado')
+        } else {
+            return res.status(400).send(`<h1>Datos incorrectos</h1>`)
+        }
+    }
+    catch (error) {
+        res.status(500).json({
+            succes: false,
+            message: error.message
+        })
+    }
+})
+
+routerSession.get('/logueado', (req, res) => {
+    const user = req.session.usuario
+    res.render('logueado', { user })
+})
+
+
+routerSession.use('/logout', async (req, res) => {
+    try {
+        req.session.destroy(err => {
+            if (err) {
+                return res.status(500).send(`<h1>No se pudo cerrar sesion</h1>`)
+            }
+        })
+        return res.status(200).send(`<h1>Hasta pronto!</h1>`)
+    } catch (error) {
+        res.status(500).json({
+            succes: false,
+            message: error.message
+        })
+    }
+})
+
+
+
+app.use('/session', routerSession)
+
+app.listen(3000, () => {
+    console.log('server running!');
+})
+
